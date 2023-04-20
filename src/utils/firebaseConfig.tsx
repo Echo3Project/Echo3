2b6d35a1cd1662da070a8890ca60a5afc97c31b0
jeanbapt-sanchez <sanchez.jeanbapt+github@gmail.com>
2023-04-20 18:13:25 +0200

    chore(db-host): Add Firebase config from web setup
    style(firebase): Lint
    
    style(firebase): Variables in .env
    
    fix(firebase): Next env on public

---
+++ src/utils/firebaseConfig.tsx
@@ -0,0 +1,28 @@
+import { getAnalytics } from '@firebase/analytics';
+import { getFirestore } from '@firebase/firestore';
+import { initializeApp } from 'firebase/app';
+
+// import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+    // Web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseDb = getFirestore(firebaseApp);
+const firebaseAnalytics =
+    firebaseApp.name && typeof window !== 'undefined'
+        ? getAnalytics(firebaseApp)
+        : null;
+
+// reportWebVitals();
+
+export { firebaseApp, firebaseDb, firebaseAnalytics };
