bff7ef17dd81ed12d60cfc71b64442aad1ae0237
Lissandre <lissandre.pasdeloup@hetic.net>
2023-06-07 16:12:51 +0200

    feat(Modal): Add modal component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { ReactElement } from 'react';
 import { Composer } from '@/components/canvas/Composer';
 import { EthereumModel } from '@/components/canvas/Models/EthereumModel';
 import { ForestModel } from '@/components/canvas/Models/ForestModel';
+import { Modal } from '@/components/dom/Elements';
 import { Three } from '@/components/helpers/R3f';
 
 export default function Page(): ReactElement {
@@ -15,7 +16,26 @@ export default function Page(): ReactElement {
                 <meta name="description" content="Echo 3 Home" />
             </Head>
             <main className="h-screen w-full flex justify-center">
-                <h1>ok</h1>
+                <Modal>
+                    <div className="p-4 flex flex-col items-center max-w-sm">
+                        <span className="text-lg mb-1">
+                            Supprimer le filtre
+                        </span>
+                        <span>(Exemple de modal)</span>
+                        <p className="text-sm text-center font-light">
+                            Êtes vous sur de vouloir supprimer ce filtre
+                            définitivement?
+                        </p>
+                    </div>
+                    <div className="mt-4 flex justify-between gap-2">
+                        <button className="py-2 w-1/2 bg-white text-black border border-black rounded-md">
+                            Annuler
+                        </button>
+                        <button className="py-2 w-1/2 bg-black text-white border border-white rounded-md">
+                            Confirmer
+                        </button>
+                    </div>
+                </Modal>
             </main>
             <Three>
                 <PerspectiveCamera
