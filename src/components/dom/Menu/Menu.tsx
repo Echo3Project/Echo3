74f9fa6bbfd2bdebeb319c0b3306782fb8c2f894
Lissandre <lissandre.pasdeloup@hetic.net>
2023-06-08 10:51:02 +0200

    feat(Menu): Add disconnected state

--- src/components/dom/Menu/Menu.tsx
+++ src/components/dom/Menu/Menu.tsx
@@ -1,43 +1,79 @@
+import clsx from 'clsx';
 import { useRouter } from 'next/router';
-import { ReactElement } from 'react';
+import { ReactElement, useContext, useEffect, useState } from 'react';
 
-import { ContribIcon, FeedIcon, ListIcon, MapIcon } from '../Elements/Icons';
+import { User } from '@/components/helpers/context/UserContext';
+import { DiscordUser } from '@/utils/discord/types';
+
+import {
+    ConnexionIcon,
+    ContribIcon,
+    FeedIcon,
+    ListIcon,
+    MapIcon,
+} from '../Elements/Icons';
 import Account from './Account';
 import Item from './Item';
 
 export default function Menu(): ReactElement {
     const notifications = 0;
     const router = useRouter();
+    const [user, setUser] = useState<DiscordUser | null>(null);
+    const userContext = useContext(User);
+
+    useEffect(() => {
+        if (!userContext) return;
+        setUser(userContext);
+    }, [userContext]);
 
     function isActive(route: string): boolean {
         return route === router.asPath ? true : false;
     }
 
+    const navClsx = clsx(
+        'backdrop-blur-lg backdrop-brightness-90 bg-lighter rounded-lg p-4 flex max-w-sm justify-between items-center text-white h-16',
+        user ? 'w-full' : 'gap-6 w-fit',
+    );
+
     return (
         <div className="fixed bottom-0 w-full flex justify-center p-8">
-            <nav className="backdrop-blur-lg backdrop-brightness-90 bg-lighter rounded-lg p-4 flex w-full max-w-sm justify-between items-center text-white h-16">
+            <nav className={navClsx}>
                 <Item route="/liste" title="Liste" active={isActive('/liste')}>
                     <ListIcon color="#ffffff" />
                 </Item>
                 <Item route="/carte" title="Carte" active={isActive('/carte')}>
                     <MapIcon color="#ffffff" />
                 </Item>
-                <Account active={isActive('/profil')} />
-                <Item
-                    route="/feed"
-                    title="Feed"
-                    active={isActive('/feed')}
-                    notifications={
-                        notifications > 0 ? notifications : undefined
-                    }>
-                    <FeedIcon color="#ffffff" />
-                </Item>
-                <Item
-                    route="/contribution"
-                    title="Contrib"
-                    active={isActive('/contribution')}>
-                    <ContribIcon color="#ffffff" />
-                </Item>
+                {user ? (
+                    <>
+                        <Account
+                            active={isActive('/profil')}
+                            avatar={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+                        />
+                        <Item
+                            route="/feed"
+                            title="Feed"
+                            active={isActive('/feed')}
+                            notifications={
+                                notifications > 0 ? notifications : undefined
+                            }>
+                            <FeedIcon color="#ffffff" />
+                        </Item>
+                        <Item
+                            route="/contribution"
+                            title="Contrib"
+                            active={isActive('/contribution')}>
+                            <ContribIcon color="#ffffff" />
+                        </Item>
+                    </>
+                ) : (
+                    <Item
+                        route="/api/auth"
+                        title="Login"
+                        active={isActive('/api/auth')}>
+                        <ConnexionIcon color="#ffffff" />
+                    </Item>
+                )}
             </nav>
         </div>
     );
