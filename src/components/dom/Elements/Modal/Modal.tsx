696d103b21809a49a52917b89197c07ef99cdd7f
Lissandre <lissandre.pasdeloup@hetic.net>
2023-06-08 11:34:53 +0200

    feat(Modal): Add open and close state on modal

--- src/components/dom/Elements/Modal/Modal.tsx
+++ src/components/dom/Elements/Modal/Modal.tsx
@@ -1,16 +1,22 @@
 import { PropsWithChildren, ReactElement } from 'react';
 import tunnel from 'tunnel-rat';
 
-type Props = PropsWithChildren;
+type Props = PropsWithChildren & {
+    active: boolean;
+};
 
 export const modal = tunnel();
 
-export default function Modal({ children }: Props): ReactElement {
+export default function Modal({ children, active }: Props): ReactElement {
     return (
         <modal.In>
-            <div className="fixed top-0 left-0 p-8 h-screen w-full flex justify-center items-center bg-opacifier pointer-events-auto z-50">
-                <div className="rounded-xl bg-white p-4">{children}</div>
-            </div>
+            {
+                active && (
+                    <div className="fixed top-0 left-0 p-8 h-screen w-full flex justify-center items-center bg-opacifier pointer-events-auto z-50">
+                        <div className="rounded-xl bg-white p-4">{children}</div>
+                    </div>
+                )
+            }
         </modal.In>
     );
 }
