e96d03b719a24034a07f10405c2366ff6e1b24b3
jeanbapt-sanchez <sanchez.jeanbapt+github@gmail.com>
2023-06-08 14:12:53 +0200

    feat(hosting): add package for host & fix node v

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['@react-three/postprocessing'],
-  images: {
-    domains: ['cdn.discordapp.com'],
-  },
+    reactStrictMode: true,
+    transpilePackages: ['@react-three/postprocessing'],
+    images: {
+        domains: ['cdn.discordapp.com'],
+        unoptimized: true,
+    },
+    server: {
+        port: 8080,
+    }
 }
 
 module.exports = nextConfig
